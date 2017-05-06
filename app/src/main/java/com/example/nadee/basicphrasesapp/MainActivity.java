5973de812ff24785e71c757d764c969f674c0506
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-05-06 11:07:38 -0500

    App complete

--- app/src/main/java/com/example/nadee/basicphrasesapp/MainActivity.java
+++ app/src/main/java/com/example/nadee/basicphrasesapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.nadee.basicphrasesapp;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        int id = view.getId();
+        String ourID = view.getResources().getResourceEntryName(id);
+        int resourceID = getResources().getIdentifier(ourID, "raw","com.example.nadee.basicphrasesapp");
+        Log.i("Resource ID: ",Integer.toString(resourceID));
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, resourceID);
+        mediaPlayer.start();
+        Log.i("BTN: "+ourID, "Tapped");
     }
 }
